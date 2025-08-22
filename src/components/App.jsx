f2b217d786791046b1c0eae7b3d75d87ff49a3fb
Fadiz1963 <mfzrk@protonmail.com>
2025-08-22 13:17:15 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar';
-import Hero from './Hero';
-import Services from './Services';
-import Sectors from './Sectors';
-import Contact from './Contact';
-import Footer from './Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Sectors from './components/Sectors';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
